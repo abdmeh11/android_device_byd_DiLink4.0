da48447e61b088e9ef5646e96dd54f1fe20179db
abdmeh11 <30433370+abdmeh11@users.noreply.github.com>
2025-08-01 17:45:50 -0700

    Update omni_DiLink4.0.mk

--- omni_DiLink4.0.mk
+++ omni_DiLink4.0.mk
@@ -9,14 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from DiLink4.0 device
 $(call inherit-product, device/byd/DiLink4.0/device.mk)
 
 PRODUCT_DEVICE := DiLink4.0
-PRODUCT_NAME := omni_DiLink4.0
+PRODUCT_NAME := twrp_DiLink4.0
 PRODUCT_BRAND := BYD-AUTO
 PRODUCT_MODEL := DiLink4.0 For BYD AUTO
 PRODUCT_MANUFACTURER := byd
